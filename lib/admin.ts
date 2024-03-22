69ba6921292dc3dda4e551bf467439f6b7694428
Hunter Macias <huntermacias@Hunters-MacBook-Pro.local>
2024-03-21 22:54:34 -0700

    free version complete

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,15 +1,15 @@
-import { auth } from "@clerk/nextjs"
+// import { auth } from "@clerk/nextjs"
 
-const adminIds = [
-  "user_2dGb6YEarBAQHrNYoB5dMtISRWK",
-];
+// const adminIds = [
+//   "user_2dGb6YEarBAQHrNYoB5dMtISRWK",
+// ];
 
-export const isAdmin = () => {
-  const { userId } = auth();
+// export const isAdmin = () => {
+//   const { userId } = auth();
 
-  if (!userId) {
-    return false;
-  }
+//   if (!userId) {
+//     return false;
+//   }
 
-  return adminIds.indexOf(userId) !== -1;
-};
+//   return adminIds.indexOf(userId) !== -1;
+// };
